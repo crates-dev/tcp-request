dd73753a0c1820029fc5556436978d161341572a
尤雨东 <root@ltpp.vip>
2025-02-04 09:10:16 +0800

    feat: sh

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git remote add ltpp ssh://git@git.ltpp.vip:66/root/tcp-request.git;
+git remote add jihulab ssh://jihulab.com:ltpp/tcp-request.git;
 git remote add origin git@github.com:ltpp-universe/tcp-request.git;
