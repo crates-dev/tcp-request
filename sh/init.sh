71ad5e8d866ebbf67abaf59ccdb9d332f82c12fc
尤雨东 <root@ltpp.vip>
2025-02-07 23:10:25 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-git remote add jihulab git@jihulab.com:ltpp-universe/tcp-request.git;
+git init;
+git config core.ignorecase false;
+git remote add gitee git@gitee.com:ltpp-universe/tcp-request.git;
 git remote add origin git@github.com:ltpp-universe/tcp-request.git;
