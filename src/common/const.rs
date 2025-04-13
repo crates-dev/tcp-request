f9ea23416e16b39d57803fff379c812b0d60abb3
尤雨东 <root@ltpp.vip>
2025-04-13 16:30:31 +0800

    feat: v2.2.0

---
+++ src/common/const.rs
@@ -0,0 +1,6 @@
+pub const EMPTY_STR: &str = "";
+pub const DEFAULT_WEB_PORT: usize = 80;
+pub const SPLIT_REQUEST: &str = "\r\n\r\n";
+pub const SPLIT_REQUEST_BYTES: &[u8] = SPLIT_REQUEST.as_bytes();
+pub const DEFAULT_BUFFER_SIZE: usize = 512_000;
+pub const DEFAULT_TIMEOUT: u64 = u64::MAX;
