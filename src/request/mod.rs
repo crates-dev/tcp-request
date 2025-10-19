2c0bf76851e4b8f01406ac0a9bb2ca46eb24d69e
尤雨东 <root@ltpp.vip>
2025-10-19 13:01:59 +0800

    feat: v2.3.4

--- src/request/mod.rs
+++ src/request/mod.rs
@@ -1,7 +1,7 @@
 pub(crate) mod config;
 pub(crate) mod error;
-pub(crate) mod request;
 pub(crate) mod request_builder;
+pub(crate) mod tcp_request;
 pub(crate) mod r#trait;
 pub(crate) mod r#type;
 
@@ -11,4 +11,4 @@ pub use r#trait::*;
 pub use r#type::*;
 
 pub(crate) use config::r#struct::*;
-pub(crate) use request::r#struct::*;
+pub(crate) use tcp_request::r#struct::*;
